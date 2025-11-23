893e8dba6bddd745249c7aaad168e3d146a0df47
mente brilante <mateustorquato21@gmail.com>
2025-11-23 04:05:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -80,6 +80,6 @@ def main():
     logger.info("Bot iniciado. Aguardando comandos...")
     
 application.run_polling()
-if_name_=='_main_': # LINHA 83
+if__name__=='__main__': # LINHA 83
     keep_alive()           # Inicia o servidor web em segundo plano
     main()                 # Inicia o bot (que chama o application.run_polling())
